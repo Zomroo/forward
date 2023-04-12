491e92e9a84c54682c49014056d8b0f7caa93ea0
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-13 01:44:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,50 @@
-import telegram
-import telegram.ext
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant
+from pyrogram.types import Message
+
 import time
 
+app = Client("my_bot", api_id=16844842, api_hash="f6b0ceec5535804be7a56ac71d08a5d4", bot_token="5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4")
 
-# Telegram API token
-token = '5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4'
+@Client.on_message(filters.command("start"))
+async def start_command(client, message):
+    await message.reply("Hi! I am a Telegram bot programmed to forward messages from Channel A to Channel B. To get started, use the /fr command.")
 
-# Initialize bot
-bot = telegram.Bot(token=token)
-
-# Channel IDs
-channel_a_id = -1001668076927
-channel_b_id = -1001713208670
-
-# Start command
-def start(update, context):
-    update.message.reply_text('Welcome to the message forwarding bot!')
-    context.user_data['start_link'] = False
-    context.user_data['end_link'] = False
-
-
-# Forward command
-def forward(update, context):
-    # Check if bot is in channel A
-    if not bot.get_chat_member(chat_id=channel_a_id, user_id=bot.id).status == 'member':
-        update.message.reply_text('Please add the bot to the source channel first.')
+@Client.on_message(filters.command("fr"))
+async def forward_messages(client, message):
+    # Check if the user is an admin in both channels
+    try:
+        admin_A = await client.get_chat_member(chat_id="-1001668076927", user_id=message.from_user.id)
+        admin_B = await client.get_chat_member(chat_id="-1001713208670", user_id=message.from_user.id)
+        if not (admin_A.status in ("creator", "administrator") and admin_B.status in ("creator", "administrator")):
+            await message.reply("Sorry, you need to be an admin in both channels to use this command.")
+            return
+    except ChatAdminRequired:
+        await message.reply("Sorry, I need to be an admin in both channels to use this command.")
         return
-    # Check if bot is in channel B
-    if not bot.get_chat_member(chat_id=channel_b_id, user_id=bot.id).status == 'member':
-        update.message.reply_text('Please add the bot to the destination channel first.')
-        return
-    # Get starting and ending message links
-    update.message.reply_text('Please provide the starting and ending message links.')
-    context.user_data['start_link'] = True
-    context.user_data['end_link'] = False
+    
+    # Ask for the starting and ending message links
+    await message.reply("Please provide me the link of the starting message in Channel A.")
+    start_link = (await app.listen(message.chat.id)).text
+    await message.reply("Please provide me the link of the last message in Channel A where I am added.")
+    end_link = (await app.listen(message.chat.id)).text
+    
+    # Get the message IDs of the starting and ending messages
+    start_msg_id = int(start_link.split("/")[-1])
+    end_msg_id = int(end_link.split("/")[-1])
+    
+    # Forward the messages in batches of 200 with a 5-minute break after each batch
+    for i in range(start_msg_id, end_msg_id+1):
+        try:
+            message = await client.get_messages(chat_id="-1001668076927", message_ids=i)
+            await client.send_message(chat_id="-1001713208670", text=message.text)
+        except Exception as e:
+            print(e)
+        
+        if i % 200 == 0:
+            time.sleep(300)
+    
+    await message.reply("Messages forwarded successfully.")
 
-# Message handler
-def message(update, context):
-    # Check if user is providing a link
-    if context.user_data.get('start_link', False):
-        context.user_data['start_link'] = False
-        context.user_data['end_link'] = True
-        context.user_data['start_message_id'] = int(update.message.text.split('/')[-1])
-        update.message.reply_text('Please provide the ending message link.')
-    elif context.user_data.get('end_link', False):
-        context.user_data['end_link'] = False
-        context.user_data['end_message_id'] = int(update.message.text.split('/')[-1])
-        update.message.reply_text('Starting message ID: {}\nEnding message ID: {}'.format(
-            context.user_data['start_message_id'],
-            context.user_data['end_message_id']
-        ))
-        # Forward messages
-        count = 0
-        message = bot.get_messages(chat_id=channel_a_id, message_id=context.user_data['start_message_id'])
-        while message.message_id <= context.user_data['end_message_id']:
-            # Forward message
-            bot.forward_message(chat_id=channel_b_id, from_chat_id=channel_a_id, message_id=message.message_id)
-            count += 1
-            # Check if break is needed
-            if count % 200 == 0:
-                update.message.reply_text('Forwarded {} messages. Taking a 5-minute break...'.format(count))
-                time.sleep(300)
-            # Get next message
-            message = bot.get_messages(chat_id=channel_a_id, message_id=message.message_id + 1)
-        # End forwarding
-        update.message.reply_text('Finished forwarding messages. Total messages forwarded: {}.'.format(count))
-
-
-
-# Main function
-def main():
-    # Create updater and dispatcher
-    updater = telegram.ext.Updater(token=token, use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Register handlers
-    dispatcher.add_handler(telegram.ext.CommandHandler('start', start))
-    dispatcher.add_handler(telegram.ext.CommandHandler('forward', forward))
-    dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, message))
-
-    # Start polling
-    updater.start_polling()
-
-    # Run until interrupted
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+app.run()
