54089d36930daa19209d83b1e8f4965eaa52d9cb
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-13 03:03:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pyrogram
 from pyrogram import filters
 
-
 api_id = 16844842 # Replace with your Telegram API ID
 api_hash = "f6b0ceec5535804be7a56ac71d08a5d4" # Replace with your Telegram API hash
 bot_token = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4" # Replace with your bot token
@@ -13,18 +12,18 @@ source_chat_id = -1001668076927
 destination_chat_id = -1001713208670
 
 # Forward the message from source chat
-@app.on_message(pyrogram.Filters.command("forward"))
+@app.on_message(filters.command("forward"))
 def forward_message(client, message):
     client.send_message(chat_id=message.chat.id, text="Please forward the message from the group you want to forward.")
 
     # Wait for the user to forward the message and get the message ID
-    @app.on_message(pyrogram.Filters.forwarded_from(source_chat_id) & pyrogram.Filters.private)
+    @app.on_message(filters.forwarded_from(source_chat_id) & filters.private)
     def forward_first_message(client, message):
         first_message_id = message.message_id
         client.send_message(chat_id=message.chat.id, text="Please forward the last message you want to forward.")
 
         # Wait for the user to forward the last message and get the message ID
-        @app.on_message(pyrogram.Filters.forwarded_from(source_chat_id) & pyrogram.Filters.private)
+        @app.on_message(filters.forwarded_from(source_chat_id) & filters.private)
         def forward_last_message(client, message):
             last_message_id = message.message_id
             client.send_message(chat_id=message.chat.id, text="Forwarding messages...")
@@ -39,7 +38,7 @@ def forward_message(client, message):
     app.stop()
 
 # Start the bot
-@app.on_message(pyrogram.Filters.command("start"))
+@app.on_message(filters.command("start"))
 def start(client, message):
     client.send_message(chat_id=message.chat.id, text="Hello! I am a bot that can forward messages from one group to another. To use me, just send /forward command and follow the instructions.")
 
