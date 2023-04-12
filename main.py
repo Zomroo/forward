b9f0278d3623ca67ab352004199f3a937285ceb4
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-13 03:34:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,77 @@
-import pyrogram
-from pyrogram import Client, filters
+import telegram
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 
-api_id = 16844842 # Replace with your Telegram API ID
-api_hash = "f6b0ceec5535804be7a56ac71d08a5d4" # Replace with your Telegram API hash
-bot_token = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4" # Replace with your bot token
+# insert your Telegram bot token here
+BOT_TOKEN = 'your_bot_token'
 
-app = Client(
-    "my_bot",
-    api_id=api_id,
-    api_hash=api_hash,
-    bot_token=bot_token
-)
+# insert the ID of the channel you want to send messages to
+CHANNEL_ID = 'your_channel_id'
 
-@app.on_message(filters.command("start"))
-def start_command(client, message):
-    client.send_message(message.chat.id, "Hello! Send me a message to forward to the database channel.")
+# function to handle the /start command
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Hi! I'm a bot that can forward your messages to a specified channel. Use the /sendmsg command to send a message.")
 
-@app.on_message(filters.private)
-def forward_to_channel(client, message):
-    # Replace 'database_channel' with the username or ID of the channel where you want to forward the messages
-    client.forward_messages('-1001713208670', message.chat.id, message.message_id)
+# function to handle the /sendmsg command
+def sendmsg(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Please enter the message you want to send to the channel.")
+    # set a "waiting_for_message" flag to indicate that the bot is waiting for a message
+    context.user_data['waiting_for_message'] = True
 
-app.run()
+# function to handle incoming messages
+def handle_message(update, context):
+    # check if the bot is waiting for a message
+    if context.user_data.get('waiting_for_message', False):
+        # check if the message being forwarded is a video, photo, or document
+        if update.message.video:
+            # forward the video to the specified channel
+            context.bot.send_video(chat_id=CHANNEL_ID,
+                                   video=update.message.video.file_id,
+                                   caption=update.message.caption)
+        elif update.message.photo:
+            # forward the photo to the specified channel
+            context.bot.send_photo(chat_id=CHANNEL_ID,
+                                    photo=update.message.photo[-1].file_id,
+                                    caption=update.message.caption)
+        elif update.message.document:
+            # forward the document to the specified channel
+            context.bot.send_document(chat_id=CHANNEL_ID,
+                                       document=update.message.document.file_id,
+                                       caption=update.message.caption)
+        else:
+            # forward the text message to the specified channel
+            context.bot.send_message(chat_id=CHANNEL_ID,
+                                      text=update.message.text)
+        # reset the "waiting_for_message" flag
+        context.user_data['waiting_for_message'] = False
+        # confirm to the user that the message was sent
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Message sent!")
+    else:
+        # if the bot is not waiting for a message, ignore the incoming message
+        pass
+
+
+def main():
+    # create a new bot object using the token
+    bot = telegram.Bot(token=BOT_TOKEN)
+    updater = Updater(token=BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # add command handlers for /start and /sendmsg
+    start_handler = CommandHandler('start', start)
+    sendmsg_handler = CommandHandler('sendmsg', sendmsg)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(sendmsg_handler)
+
+    # add a message handler to handle incoming messages
+    message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
+    dispatcher.add_handler(message_handler)
+
+    # start the bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
